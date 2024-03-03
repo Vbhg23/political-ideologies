ec7787b00c5d0b417a3999a95a7fcd5b43e129e6
f1lded <128279948+Vbhg23@users.noreply.github.com>
2024-03-03 12:56:13 +0600

    version #2

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,6 +3,7 @@ from flask import render_template
 
 def create_app():
     app = Flask(__name__, instance_relative_config = True)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
 
     @app.route("/")
     def display_main_page():
@@ -11,5 +12,9 @@ def create_app():
     @app.route("/nazism")
     def display_nazism():
         return render_template("nazism.html")
+    
+    @app.route("/communism")
+    def display_communism():
+        return render_template("communism.html")
     
     return app
